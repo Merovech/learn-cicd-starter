28b64e4e92c83b8c72037d0487bc04d010fb1979
Merovech <ari.roth@gmail.com>
2026-04-27 15:41:41 -0700

    Fixes

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestGetAPIKey(t *testing.T) {
 	req, _ := http.NewRequest("GET", "https://nowhere.mil.gov.sp.it.fu", nil)
-	req.Header.Set("Authorization", "ApiKey 123456")
+	req.Header.Set("Authorization", "ApiKey 12345")
 	actual, _ := GetAPIKey(req.Header)
 	if actual != "12345" {
 		t.Fail()
